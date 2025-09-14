6f1d49cb5e65707c9665a2ebeb834d944fee8027
longhoney <long.711.sg@gmail.com>
2025-09-14 17:06:11 +0700

    Ham tang toc khong phu hop

--- examples/Gamepad_Shorten/Gamepad_Shorten.ino
+++ examples/Gamepad_Shorten/Gamepad_Shorten.ino
@@ -9,34 +9,59 @@
 #define CUSTOM_SETTINGS
 #define INCLUDE_GAMEPAD_MODULE
 #include <Dabble.h>
+
+//Define Pin
+  // Motor A connections
+  int enA = 9; //tháo Jumper sẵn
+  int in1 = 8;
+  int in2 = 7;
+  // Motor B connections
+  int enB = 3; //tháo Jumper sẵn
+  int in3 = 5;
+  int in4 = 4;
+  //UART for MKE-M15
+  int RxUno = 13;
+  int TxUno = 12;
+
 void setup() {
-  // put your setup code here, to run once:
+  // Setup Bluetooth
   Serial.begin(9600);      // make sure your Serial Monitor is also set at this baud rate.
-  Dabble.begin(9600, 13, 12);      //Enter baudrate of your bluetooth.Connect bluetooth on Bluetooth port present on evive.
-  //RX of Uno = D13, TX of Uno = D12
+  Dabble.begin(9600, RxUno, TxUno);      //Enter baudrate of your bluetooth.Connect bluetooth on Bluetooth port present on evive.
+
+  // Set all the motor control pins to outputs
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
+  Cat_stop(); //Turn off all motors
 }
 
 void loop() {
   Dabble.processInput();             //this function is used to refresh data obtained from smartphone.Hence calling this function is mandatory in order to get data properly from your mobile.
-  Serial.print("KeyPressed: ");
+  //Serial.print("KeyPressed: ");
   if (GamePad.isUpPressed())
   {
-    Serial.print("UP");
+    //Serial.print("UP");
+    fwd_acclerate();
   }
 
   if (GamePad.isDownPressed())
   {
-    Serial.print("DOWN");
+    //Serial.print("DOWN");
+    bwd_acclerate();
   }
 
   if (GamePad.isLeftPressed())
   {
-    Serial.print("Left");
+    //Serial.print("Left");
   }
 
   if (GamePad.isRightPressed())
   {
-    Serial.print("Right");
+    //Serial.print("Right");
   }
 
   if (GamePad.isSquarePressed())
@@ -51,7 +76,8 @@ void loop() {
 
   if (GamePad.isCrossPressed())
   {
-    Serial.print("Cross");
+    //Serial.print("Cross");
+    Cat_stop(); //Turn off all motors
   }
 
   if (GamePad.isTrianglePressed())
@@ -68,7 +94,45 @@ void loop() {
   {
     Serial.print("Select");
   }
-  Serial.print('\n'); //tao khoang cach 1 dong
-  //Serial.print('\t');
-  Serial.println(); //tu dong xuong dong
+  //Serial.print('\n'); //tao khoang cach 1 dong
+    //Serial.print('\t');
+  //Serial.println(); //tu dong xuong dong
+}
+
+//========Cac ham dieu khien xe========//
+void fwd_acclerate(){
+  // Turn on motor A & B and go forward
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+
+  // Accelerate from zero to maximum speed
+  for (int i = 0; i < 256; i++) {
+    analogWrite(enA, i);
+    analogWrite(enB, i);
+    delay(20);
+  }
+}
+
+void bwd_acclerate(){
+  // Turn on motor A & B and go forward
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+
+  // Accelerate from zero to maximum speed
+  for (int i = 0; i < 256; i++) {
+    analogWrite(enA, i);
+    analogWrite(enB, i);
+    delay(20);
+  }
+}
+
+void Cat_stop(){
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
 }
--- examples/examples.md
+++ examples/examples.md
@@ -1,2 +1,6 @@
 `2025-09-13_17h00`
 - Tôi giao tiếp với MKE-M15 không được. Điện thoại kết nối Bluetooth với mạch thành công nhưng nhấn nút trên Gamepad thì không trả về kết quả đã nhấn phím trên Serial Monitor
+
+`2025-09-14_17h00`
+- Tôi chèn code tăng tốc - tiến phía trước vào "lệnh UP" thành công --> Động cơ phải trên ban đầu không chạy ở chu kỳ PWM thấp
+- Tôi dùng "lệnh X" để dừng động cơ --> Vấn đề xảy ra: Phải chạy xong chương trình tăng tốc thì xe mới dừng
